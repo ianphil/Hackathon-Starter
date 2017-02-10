c5f18a4a6dbed3addf44741de5c2a1006aefa278
tripdubroot <ian.philpot@microsoft.com>
2017-02-09 23:04:27 -0500

    project init node/dotnetcore

---
+++ dotnetcore/cog_svc/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Net.Http;
+using System.Text;
+using System.Net.Http.Headers;
+using Microsoft.AspNetCore.WebUtilities;
+using System.IO;
+using Newtonsoft.Json;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication
+{
+    public class Program
+    {
+        private static HttpClient client = new HttpClient();
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Please paste URL: ");
+            var pictureUrl = Console.ReadLine();
+            Console.WriteLine("URL is: " + pictureUrl);
+
+            CallService(pictureUrl).Wait();
+        }
+
+        private static async Task CallService(string pictureUrl)
+        {
+            var queryString = QueryHelpers.ParseQuery(string.Empty);
+
+            cogSecret cogSecret;
+            using(var reader = new StreamReader(File.OpenRead("secret.json")))
+            {
+                var data = reader.ReadToEnd();
+                cogSecret = JsonConvert.DeserializeObject<cogSecret>(data);
+            }
+
+            client.DefaultRequestHeaders.Add("Ocp-Apim-Subscription-Key", cogSecret.cog_key);
+
+            queryString["visualFeatures"] = "Categories,Description";
+            var uri = "https://westus.api.cognitive.microsoft.com/vision/v1.0/analyze?" + queryString;
+
+            HttpResponseMessage response;
+
+            var messageBody = new MessageBody { url = pictureUrl };
+            var pictureJson = JsonConvert.SerializeObject(messageBody);
+            byte[] byteData = Encoding.UTF8.GetBytes(pictureJson);
+
+            using (var content = new ByteArrayContent(byteData))
+            {
+               content.Headers.ContentType = new MediaTypeHeaderValue("application/json");
+               response = await client.PostAsync(uri, content);
+            }
+
+            var responseData = response.Content.ReadAsStringAsync();
+            Console.WriteLine(responseData.Result);
+            
+            Console.WriteLine("\nPress any key to continue...");
+            Console.ReadKey();
+        }
+    }
+
+    public class MessageBody
+    {
+        public string url;
+    }
+
+    public class cogSecret
+    {
+        public string cog_key {get; set;}
+    }
+}
